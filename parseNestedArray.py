93e301a9787df56cf449edf7a2201d0179a97c83
Maninderpal Singh <moneysingh1991@users.noreply.github.com>
2019-06-09 22:29:07 -0700

    file added
    This python program will print array of element in nested array

---
+++ parseNestedArray.py
@@ -0,0 +1,22 @@
+
+# This function
+#argument 1:  nested list of intger
+#argument 2 : list to return
+#return list
+def flatten(array,returnList):
+   
+    for item in array:
+        if isinstance(item,int):
+            returnList.append(item)
+        else:
+            flatten(item,returnList)
+   
+    return returnList
+
+
+#Testing
+print(flatten([0, [ [1,2], [ 3, [ 4, [5, 6] ] ] ] ],[])) 
+print(flatten([[1,2,[3]],4],[]))
+print(flatten([],[]))
+
+
